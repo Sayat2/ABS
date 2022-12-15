6d9e4420d3d99498875ed6e66c34554e9e0dce10
Sayat <azhen03@mail.ru>
2022-12-15 13:06:46 +0600

    login & register page files

---
+++ admin_page.php
@@ -0,0 +1,23 @@
+<?php
+@include 'config.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">  
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Good</title>
+   <link rel="stylesheet" href="css/style.css">
+</head> 
+<body>
+   <div class="container">
+      <div class="content">
+         <h3>Laboratory Work 8</h3>
+         <p>✅ You are login successfully✅ </p>
+      </div>
+   </div>
+</body>
+</html>
