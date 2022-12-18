435c01f3f9e7e374e0f233faf15b78320d0927bf
Bayan <210103228@stu.sdu.edu.kz>
2022-12-18 16:38:44 +0600

    Commit

---
+++ home.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/slidehome1.png) no-repeat">
+            <div class="content">
+               <span>with online courses</span>
+               <h3>Learn programming</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/slidehome2.jpg) no-repeat">
+            <div class="content">
+               <span> from real college courses</span>
+               <h3>SDU, Harvard, MIT, and more</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/slidehome3_2.png) no-repeat">
+            <div class="content">
+               <span>almost anyone can learn to hack</span>
+               <h3>Learn to Hack</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<section class="services">
+
+   <h1 class="heading-title">Course Categories</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/html_css.png" alt="">
+         <h3>HTML/CSS</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/js.png" alt="">
+         <h3>JavaScript</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sql.png" alt="">
+         <h3>SQL</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/php.png" alt="">
+         <h3>PHP</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/python.png" alt="">
+         <h3>Python</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/javva-rbg.png" alt="">
+         <h3>Java</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/AiBaSa.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>We are Legends of Jaksylyk 😎</p>
+      <p>👩‍💻Aidana Bagasharova</p>
+      <p>👩‍💻Bayan Berdikozha</p>
+      <p>👨‍💻Azhen Sayat</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/jsc.png" alt="">
+         </div>
+         <div class="content">
+            <a href="js_page.php"><h3>Javascript for Beginners</h3></a>
+            <p>Learn javascript online and supercharge your web design with this Javascript for beginners training course.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/htmlcss.jpg" alt="">
+         </div>
+         <div class="content">
+            <a href="htmlcss_page.php"><h3>HTML and CSS for Beginners</h3></a>
+            <p>HTML and CSS for Beginners course will give your all the knowledge you need to master HTML and CSS easily and quickly.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+   
+      <div class="box">
+         <div class="image">
+            <img src="images/nextjs2.png" alt="">
+         </div>
+         <div class="content">
+            <a href="nextjs.php"><h3>Complete Next.js Course For Beginners - All Concepts</h3></a>
+            <p>Learn NextJS from the Scratch and build production-ready, fullstack ReactJS apps with the NextJS framework!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<section class="home-offer">
+   <div class="content">
+      <h3>upto 60% off</h3>
+      <p>Winter promotion: up to 60% discount%</p>
+      <p> Get 2 for the price of one and 5 online courses as a gift</p>
+      <a href="buy.php" class="btn">buy now</a>
+   </div>
+</section>
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
