22deabd86073b2cc8c11a442c0c4d91a04a11fc1
Bayan <210103228@stu.sdu.edu.kz>
2022-12-18 16:56:40 +0600

    Adding js page php

--- js_page.php
+++ js_page.php
@@ -6,19 +6,17 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>ABS Education</title>
 
-   <!-- swiper css link  -->
+   
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-   <!-- font awesome cdn link  -->
+   
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
    <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
-   <!-- custom css file link  -->
+   
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
    
-<!-- header section starts  -->
 
 <section class="header">
 
