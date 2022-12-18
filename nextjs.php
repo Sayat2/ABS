e451a8bb89c7390369041abc729353173158b28a
Bayan <210103228@stu.sdu.edu.kz>
2022-12-18 17:03:50 +0600

    Adding nextjs php

---
+++ nextjs.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+
+   
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>Next.js</h1>
+</div>
+
+<section class="about">
+   <div class="image">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/__mSgDEOyv8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   </div>
+
+   <div class="content">
+      <h3>Next.js</h3>
+      <p>Welcome to the ABS Education School course!To effectively use Next.js, it helps to be familiar with JavaScript, React, and related web development concepts. But JavaScript and React are vast topics. How do you know when you’re ready to learn Next.js?</p>
+      <p>Welcome to the Next.js Foundations course! This beginner-friendly, example-led course will guide you through the prerequisite knowledge for Next.js. You will build a simple project step-by-step; starting with a JavaScript application, then migrating it to React and Next.js.</p>
+   </div>
+</section>
+
+<section class="about">
+   <div class="content">
+      <p>Next.js is a flexible React framework that gives you building blocks to create fast web applications.</p>
+      <p>But what exactly do we mean by this? Let's spend some time expanding on what React and Next.js are and how they can help.</p>
+   </div>
+</section>
+
+   <section class="about">
+   <div class="image">
+      <img src="images/nextjs3.jpeg" alt="" class="htmlcssjsphoto">
+   </div>
+   </section>
+
+   <section class="about">
+   <div class="content">
+        <h3>Building Blocks of a Web Application</h3>
+        <p>There are a few things you need to consider when building modern applications. Such as: </p>
+    
+        <li><b>User Interface</b> - how users will consume and interact with your application.</li>
+        <li><b>Routing</b> - how users navigate between different parts of your application.</li>
+        <li><b>Data Fetching</b> - where your data lives and how to get it.</li>
+        <li><b>Rendering</b> - when and where you render static or dynamic content.</li>
+        <li><b>Integrations</b> - what third-party services you use (CMS, auth, payments, etc) and how you connect to them.</li>
+        <li><b>Infrastructure</b> - where you deploy, store, and run your application code (Serverless, CDN, Edge, etc).</li>
+        <li><b>Performance</b> - how to optimize your application for end-users.</li>
+        <li><b>Scalability</b> - how your application adapts as your team, data, and traffic grow.</li>
+        <li><b>Developer Experience</b> - your team's experience building and maintaining your application.</li>
+    
+        <p> For each part of your application, you will need to decide whether you will build a solution yourself or use other tools such as libraries and frameworks.</p>
+        <br><br>
+        <h3>What is React?</h3>
+        <p> <a href="https://beta.reactjs.org/">React</a> is a JavaScript library for building interactive user interfaces.</p>
+        <p>HTML is supported by every single browser and is established on pretty much every webpage in existence. You don't need any licenses, you don't need to pay for it, and it can be pretty easy to learn and code. If we can compare a webpage to the human body, then HTML is the bones of the body. By user interfaces, we mean the elements that users see and interact with on-screen.</p>
+        <div class="image">
+            <img src="images/nextjs4.png" alt="" class="htmlcssjsphoto">
+        </div>
+        <p>By library, we mean React provides helpful functions to build UI, but leaves it up to the developer where to use those functions in their application.</p>
+        <p>Part of React’s success is that it is relatively unopinionated about the other aspects of building applications. This has resulted in a flourishing ecosystem of third-party tools and solutions.</p>
+        <p>It also means, however, that building a complete React application from the ground up requires some effort. Developers need to spend time configuring tools and reinventing solutions for common application requirements.</p>
+   </div>
+</section>
+
+
+<section class="about">
+   <div class="content  ">
+        <h3>What is Next.js?</h3>
+        <p>Next.js is a React framework that gives you building blocks to create web applications.</p>
+        <p>By framework, we mean Next.js handles the tooling and configuration needed for React, and provides additional structure, features, and optimizations for your application.</p>
+        <div class="image">
+            <img src="images/nextjs5.png" alt="" class="htmlcssjsphoto">
+        </div>
+        <p>You can use React to build your UI, then incrementally adopt Next.js features to solve common application requirements such as routing, data fetching, integrations - all while improving the developer and end-user experience.</p>
+        <p>Whether you're an individual developer or part of a larger team, you can leverage React and Next.js to build fully interactive, highly dynamic, and performant web applications. </p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fa-brands fa-hackerrank"></i>
+                <span>hackerrank</span>
+            </div>
+            <div class="icons">
+                <i class="fa-brands fa-wolf-pack-battalion"></i>
+                <span>wolf</span>
+            </div>
+        </div>
+   </div>
+</section>
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
