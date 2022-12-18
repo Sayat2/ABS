716379e0d7224ebb3854e6499c578208ebf77983
Bayan <210103228@stu.sdu.edu.kz>
2022-12-18 17:00:23 +0600

    Adding html css page php

---
+++ htmlcss_page.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>HTML and CSS</h1>
+</div>
+
+<section class="about">
+   <div class="image">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/G3e-cpL7ofc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   </div>
+
+   <div class="content">
+      <h3>HTML And CSS For Beginners</h3>
+      <p>Welcome to the ABS Education School course! What are HTML and CSS used for? The basics of coding for the web</p>
+      <p>When it comes to building web pages, the coding language can, at first glance, appear incredibly complex. It may seem rather daunting having to take all this new information on board, but by breaking down each of the different elements involved, you’ll find that you can make sense of all these essential pieces of the puzzle.</p>
+   </div>
+</section>
+
+<section class="about">
+   <div class="content">
+      <p>If you're thinking of building a website, then you'll almost certainly have come across the acronyms HTML and CSS. These two essential tools form some of the foundations of any webpage. However, although they're often used in conjunction with each other, they both serve very different purposes when it comes to web coding.</p>
+      <p>The key differences lie in their implementation, ease of use, their various features, and how they are structured, but at the end of the day, both HTML and CSS are the keys to unlocking how the World Wide Web works. Let's dive into the details of both HTML and CSS, what they're used for, and how to learn HTML and CSS.  </p>
+   </div>
+</section>
+
+   <section class="about">
+   <div class="image">
+      <img src="images/htmlcssph.png" alt="" class="htmlcssjsphoto">
+   </div>
+   </section>
+
+   <section class="about">
+   <div class="content  ">
+        <h3>What is HTML?</h3>
+        <p>Let's start by breaking down exactly what the acronym 'HTML' stands for. HTML stands for HyperText Markup Language. HyperText is effectively a text within a text, and a markup language is a language understood by computers, designed to essentially describe webpages and make the text you use more interactive.</p>
+        <p> It was originally invented back in 1989 as the publishing language of the web, and to put it very simply, it is the first building block in creating a webpage. When you first come across the world of HTML, you'll likely encounter a whole ream of terms and phrases that are somewhat baffling too. The three main ones are elements, tags, and attributes.  </p>
+        <p>HTML5 is the latest 'version' of HTML, and knowledge in this area is a growing trend in the job market. By and large, it has the same functionality as standard HTML but is much more dynamic and uses much less code to build something fantastic. </p>
+       <br>  <br>
+        <h3>What is HTML used for?</h3>
+        <p>In essence, HTML is used for creating the primary content of a webpage, giving it structure. You start by writing words, then apply tags or elements to these words. The web browser then reads this and can then understand the heading of a page, any paragraphs, and where the page starts and finishes, thus filling your web page with content. </p>
+        <p>HTML is supported by every single browser and is established on pretty much every webpage in existence. You don't need any licenses, you don't need to pay for it, and it can be pretty easy to learn and code. If we can compare a webpage to the human body, then HTML is the bones of the body.</p>
+        <div class="icons-container">
+            <div class="icons">
+            <i class="fa-brands fa-html5"></i>
+            <span>HTML</span>
+            </div>
+            <div class="icons">
+                <i class="fa-solid fa-sitemap"></i>
+                <span>browser</span>
+            </div>
+        </div>
+   </div>
+</section>
+
+
+<section class="about">
+   <div class="content  ">
+        <h3>What is CSS? </h3>
+        <p>CSS is the acronym for Cascade Styling Sheets. In short, it is a sheet style language, which is a type of language you can use to describe the presentation of a markup language - in this case, to describe the movements of HTML. It effectively determines how the building blocks, as laid by HTML, are decorated and presented to the user.</p>
+        <p> CSS was first created around 1996 to make sense of HTML and make the webpage look and feel amazing. Just as with any language, you have to write CSS, and knowing how we write CSS is a skill that any webpage developer needs to have. Easy to maintain, CSS is the second part of the two-part tool kit for webpage creation.     </p>
+        <p>Web accessibility is really important to many employers and companies nowadays, and there is fierce competition in creating the most accessible and well-designed webpage. Learning some software development fundamentals involving CSS can help you learn how to create accessible web pages.</p>
+       <br>  <br>
+        <h3>What is CSS used for? </h3>
+        <p>If HTML is the bones of the body, then CSS is the skin that covers it. It's used for background colour, styling, layout, borders, shadowing - all the essential design bits and bobs that make a webpage look slick and smart. CSS enables you to distinguish between presentation and content by modifying the design and display of HTML elements.</p>
+        <p>Presentation and ease of use are a couple of the main things that CSS has bought to web design by translating the way content looks on a webpage and what else goes on it to complement that content. While frequently used in conjunction with HTML, it is actually independent of it, and can be used with any XML-based markup language. </p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fa-brands fa-css3-alt"></i>
+                <span>CSS</span>
+            </div>
+            <div class="icons">
+                <i class="fa-solid fa-pen-nib"></i>
+                <span>Design</span>
+            </div>
+        </div>
+   </div>
+</section>
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
