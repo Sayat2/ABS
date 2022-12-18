fbc4a4db5b4f9102554b008d73717abba0e13f75
Sayat <azhen03@mail.ru>
2022-12-18 16:51:09 +0600

    add package.php file

---
+++ package.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<section class="packages">
+
+   <h1 class="heading-title">top courses</h1>
+
+   <div class="box-container">
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/jsc.png" alt="">
+         </div>
+         <div class="content">
+         <a href="js_page.php"><h3>Javascript for Beginners</h3></a>
+            <p>Learn javascript online and supercharge your web design with this Javascript for beginners training course.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/htmlcss.jpg" alt="">
+         </div>
+         <div class="content">
+         <a href="htmlcss_page.php"><h3>HTML and CSS for Beginners</h3></a>
+            <p>HTML and CSS for Beginners course will give your all the knowledge you need to master HTML and CSS easily and quickly.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/nextjs2.png" alt="">
+         </div>
+         <div class="content">
+            <a href="nextjs.php"><h3>Complete Next.js Course For Beginners - All Concepts</h3></a>
+            <p>Learn NextJS from the Scratch and build production-ready, fullstack ReactJS apps with the NextJS framework!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/bootcamp.png" alt="">
+         </div>
+         <div class="content">
+            <a href="bootcamp.php"><h3>The Web Developer Bootcamp 2022</h3></a>
+            <p>COMPLETELY REDONE - The only course you need to learn web development - HTML, CSS, JS, Node, and More!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/react.png" alt="">
+         </div>
+         <div class="content">
+            <a href="react.php"><h3>Build React Applications</h3></a>
+            <p>create a portfolio website to show the projects.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/wordpress.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>How to Make Income Making Blogging with WordPress</h3>
+            <p>Build Blogging WordPress website</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/vue.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Vue - The Complete Guide</h3>
+            <p>Vue.js is an awesome JavaScript Framework for building Frontend Applications! VueJS mixes the Best of Angular + React!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/mern.png" alt="">
+         </div>
+         <div class="content">
+            <h3>Modern MERN Stack | React Node Express MongoDB</h3>
+            <p>Become A Full Stack Javascript Developer</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/pd.png" alt="">
+         </div>
+         <div class="content">
+            <h3>Python and Django</h3>
+            <p>Learn to build websites with HTML , CSS , Bootstrap , Javascript , jQuery , Python 3 , and Django!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/sass.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Advanced CSS and Sass</h3>
+            <p>master flexbox, CSS Grid, responsive design, and so much more.</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/TypeScript.jpeg" alt="">
+         </div>
+         <div class="content">
+            <h3>Understanding TypeScript - 2023 Edition</h3>
+            <p>Boost your JavaScript projects with TypeScript</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/angular.png" alt="">
+         </div>
+         <div class="content">
+            <h3>Angular - The Complete Guide</h3>
+            <p>Master Angular (formerly "Angular 2") and build awesome</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/sqll.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>SQL for Beginners</h3>
+            <p>Understand SQL using the MySQL database!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/git.png" alt="">
+         </div>
+         <div class="content">
+            <h3>Git & GitHub</h3>
+            <p>Learn the complete Git & GitHub environment & master managing your projects and your team like a professional developer!</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/Postgre.png" alt="">
+         </div>
+         <div class="content">
+            <h3>PostgreSQL and Oracle SQL</h3>
+            <p>Learn popular SQL dialects, PostgreSQL & Oracle SQL</p>
+            <a href="buy.php" class="btn">buy now</a>
+         </div>
+      </div>
+
+   </div>
+
+      <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
