b78c35bab4c51ca673d863ca80d5fd1193abcc1b
Sayat <azhen03@mail.ru>
2022-12-19 01:27:02 +0600

    add react page

---
+++ react.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>React js</h1>
+</div>
+
+<section class="about">
+   <div class="image">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-ERWlp828kY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+   <div class="content">
+      <h3>Build React Applications</h3>
+      <p>Welcome to the ABS Education School course!</p>
+      <p>You most likely use React apps everyday, while watching your favorite TV shows, browsing through social media and checking your email. Find out which famous companies built their apps with ReactJS and why.</p>
+   </div>
+</section>
+
+<section class="about">
+   <div class="content">
+        <h3>Introduction</h3>
+      <p>Let’s face it – the digital world is changing as we speak. In such reality, it is definitely hard to adapt to trends. However, that is exactly what the big names in the industry are doing.</p>
+      <p>Major apps like Facebook, Instagram, Netflix and others are constantly improving their experience and adapting to new frameworks and trends.</p>
+      <p>As of recently, there is a big word of mouth going around ReactJS and its impressive features.</p>
+      <p>In case you still haven’t heard about the potential of ReactJS, they are numerous. Basically, React has become very popular among developers and there are lots of resources that make most of its technical merits – just like NodeJS and numerous other related JavaScript frameworks/tools.</p>
+      <p>The proof for its popularity is best described by the apps that are using ReactJS – and today, we are showing you the list of most impressive apps based on ReactJS.</p>
+   </div>
+</section>
+
+   <section class="about">
+   <div class="image">
+      <img src="images/React-app.jpg" alt="" class="htmlcssjsphoto">
+   </div>
+   </section>
+
+   <section class="about">
+   <div class="content  ">
+        <h3>What is React?</h3>
+        <p>To cut a long story short, React is a JavaScript library for building User Interfaces. Very often it’s misinterpreted as a tool, framework, language. <a href="https://brainhub.eu/library/top-react-development-companies"> Vendors working with React</a> are able to create large web applications which use data that can change over time, without reloading the page. Its main goal is to be fast, simple and scalable. React processes only user interface in applications. It is simply a JavaScript runtime. <b> Lightweight, fast and modern</b> way to execute code on your computer.</p>
+        
+        <br><br>
+        <h3>Why do internet giants choose React apps?</h3>
+        <p><b>#1 Facebook</b></p>
+        <p>Although partially, Facebook is making use of ReactJS. Their webpage is built with React, as the script that is blended in the application code. The mobile app is also built with a version of React called React Native which is similar, although responsible for displaying the iOS and Android native components instead of the DOM elements. </p>
+        <p>Interestingly, Facebook was the place where the ReactJS library was initially created, which is why it is obvious for the app to use it. Facebook currently opened a beta of its completely rewritten ReactJS, called React Fiber.</p>
+        <div class="image">
+            <img src="images/facebook.png" alt="" class="htmlcssjsphoto">
+        </div>
+
+        <br><br><br><br>
+        <p><b>#2 Instagram </b></p>
+        <p>The use of ReactJS within Instagram is huge. A proof for that is the numerous features including the geolocations, Google Maps APIs, search engine accuracy as well as tags that pop out without hashtags. IT is all there in the API of the app – and is really impressive. </p>
+        <p> Instagram is completely based on the ReactJS library and has let fans fully adapt to its amazing features.</p>
+        <div class="image">
+            <img src="images/inst.jpg" alt="" class="htmlcssjsphoto">
+        </div>
+
+        
+        <br> <br><br> <br>
+        <p><b>#3 Netflix </b></p>
+        <p> The React version works with Netflix too – specifically on their platform called Gibbon which is used for low-performance TV devices instead of the DOM used in web browsers. Netflix has even published an official blog post explaining how the ReactJS library helps its startup speed, runtime performance, modularity, and various other advantages.</p>
+        <p>As the UI engineers at Netflix state in the blog post: </p>
+        <p><i><b>Our decision to adopt React was influenced by a number of factors, most notably: 1) startup speed, 2) runtime performance, and 3) modularity.</b></i></p>
+        <div class="image">
+            <img src="images/netflix.png" alt="" class="htmlcssjsphoto">
+        </div>
+        
+        <br> <br><br> <br>
+        <p><b>#4 New York Times </b></p>
+        <p> A couple of months ago, the New York Times has designed a great new project that simulates different looks of stars on Oscar red carpet. Obviously, this project’s interface was built in React and lets users filter the gallery of different photos from 19 years in a nice way. The re-rendering on this project is only one of the impressive features we can thank ReactJS for.</p>
+        <p> Justin Heideman backs these reasons up in his blog post on NYTimes Open, stating that:</p>
+        <p><i><b>Within our app we create lightweight, single responsibility Stores. A Store is responsible for managing a particular data request.</b></i></p>
+        <div class="image">
+            <img src="images/nyt.png" alt="" class="htmlcssjsphoto">
+        </div>
+
+        
+        <br> <br><br> <br>
+        <p><b> #5 Yahoo! Mail</b></p>
+        <p>Surprisingly (or not), Yahoo!’s mail client also uses React. Since Facebook owns Yahoo! nowadays, the idea of a solid and unified architecture is there, and that is why React was incorporated in as many bits and pieces as possible. The architecture that is specifically built with React as a piece of it can be seen here – and the Yahoo! developers are summing it up as easier to work with the code and a lot better. </p>
+        <p> The engineers working on the Yahoo Mail platform needed a lot of upgrades. As they say in a blog post on Tumblr:</p>
+        <p><i><b>For the next generation Yahoo Mail platform, we wanted: 1.Predictable flow ~ Easy Debugging 2.Independently deployable components 3.Shorter learning curve 4.No dependency on large platform libraries </b></i></p>
+        <div class="image">
+            <img src="images/yahoo.png" alt="" class="htmlcssjsphoto">
+        </div>
+        
+        <br> <br><br> <br>
+        <p><b> #6 Khan Academy</b></p>
+        <p> A lot of parts of Khan Academy are based on React currently. One of their JavaScript developers, Joel Burget, shared his personal experience with the ReactJS library and how it differentiates from the traditional Backbone script they were previously using.</p>
+        <p>He obviously defines it as a worthy upgrade and carefully goes through most of the important features – including the element changing in an efficient way and the elimination of unnecessary re-renders. </p>
+        <div class="image">
+            <img src="images/khanacademy.png" alt="" class="htmlcssjsphoto">
+        </div>
+        
+        <br><br><br><br><br>
+        <p><b>#7 WhatsApp </b></p>
+        <p>Although there were several betas before it was officially launched, the WhatsApp uses ReactJS for building user interfaces from Facebook, just like it uses Underscore.js and Velocity.js as some of its most efficient engines. </p>
+        <p>As of recently, the all-new WhatsApp Web app has also been using React, just like the Facebook web experience mentioned above
+
+        </p>
+        <div class="image">
+            <img src="images/whats.png" alt="" class="htmlcssjsphoto">
+        </div>
+
+        <br><br><br><br><br>
+        <p><b> #8 Vivaldi Browser</b></p>
+        <p> One of the technologies behind the popular Vivaldi Browser is the ReactJS library. The engine that this browser is using is named ‘Blink’ and is pretty much the same as Google’s Chrome, built on HTML5, ReactJS, JS, CSS3 and many other engines.</p>
+        <p> </p>
+        <div class="image">
+            <img src="images/Vivaldi.png" alt="" class="htmlcssjsphoto">
+        </div>
+        
+        <br><br><br><br><br>
+        <p><b>#9 Codecademy </b></p>
+        <p> As of August 2014, Codecademy has decided to fully incorporate Facebook’s library. ReactJS was obviously a part of it – and is still one of the key scripts that are based on the app.</p>
+        <p> From the header to the menu and even the navigation, the ReactJS use is all there on Codeacademy, created as a logical solution that packs all the components for the various pieces.</p>
+        <p>According to everyone at Codeacademy, some of the aspects of React that they appreciate include the fact that the <b> script is battle-tested, easy to think about, makes SEO easy and is compatible with legacy code and flexible enough for the future.</b></p>
+        <div class="image">
+            <img src="images/codeacademy.jpg" alt="" class="htmlcssjsphoto">
+        </div>
+
+        <br><br><br><br><br>
+        <h3>React apps = perfect user experience</h3>
+        <p>In the end, the reduced risk, the improved efficiency and effectiveness ReactJS developers report, and numerous organizational benefits have all been reasons for the big names in apps to upgrade to ReactJS and exploit the amazing benefits offered by this script.</p>
+        <p>And even though every framework upgrade takes its toll regarding time and costs, it is absolutely worth it when it comes to creating the perfect user experience – be it on a web or a mobile app.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fa-brands fa-react"></i>
+            <span>React</span>
+            </div>
+            <div class="icons">
+                <i class="fa-brands fa-reacteurope"></i>
+                <span>React Europe</span>
+            </div>
+        </div>
+   </div>
+</section>
+
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
