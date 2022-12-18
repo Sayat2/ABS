d3a63f52ca7a68c6b3365f6cc9394ab3169bf895
Bayan <210103228@stu.sdu.edu.kz>
2022-12-18 17:02:10 +0600

    Adding bootcamp php

---
+++ bootcamp.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>Web Developer Bootcamp</h1>
+</div>
+
+<section class="about">
+   <div class="image">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/NqDEt2FbvCA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   </div>
+
+   <div class="content">
+      <h3>The Web Developer Bootcamp 2022</h3>
+      <p>Welcome to the ABS Education School course! </p>
+      <p>The Web Developer Bootcamp by Colt Steele is one of the comprehensive, clear, engaging, and up-to-date courses to learn essential web development technologies in 2022. Definitely worth it.</p>
+   </div>
+</section>
+
+<section class="about">
+   <div class="content">
+      <p>Hello guys, if you want to learn web development and looking for an amazing course to start with, or you have chosen the 2022 Web Developer Bootcamp by Colt Steele and wondering whether it's the right course for you not then you have come to the right place.</p>
+      <p>Earlier, I have shared the free web development courses and in this article, I am going to review Udemy’s most popular web development course, the 2022 Web Developer Bootcamp by Colt Steele, one of my favorite instructors on Udemy.</p>
+   </div>
+</section>
+
+   <section class="about">
+   <div class="image">
+      <img src="images/Coding-Bootcamp.png" alt="" class="htmlcssjsphoto">
+   </div>
+   </section>
+
+   <section class="about">
+   <div class="content  ">
+        <p>Learning web development nowadays is such a huge work to do because you are going to learn not only to design the front-end or the web interface but you are also going to learn the back-end as well as design and build the database system for that website or online service and that’s what’s known as Full-stack web developer.</p>
+        <p> Hiring a Full-stack web developer is an essential part of any company or profit organization and they reduce the costs of hiring many people to create the whole website from the front-end, the back-end, and the database.</p>
+        <p>Glassdoor has estimated that the average salary in the USA for a full-stack web developer is $68,524 a year.</p>
+        <p>Finding the right course to learn all these skills can be a tough task but in this article, I have found literally the best one to learn all that you need to start a career in this field and this course called The Web Developer Bootcamp hosted on udemy platform and this article is a review for that course. Stay tuned to see what you are going to learn along the way when enrolling in this Bootcamp.</p>
+       <br>  <br>
+        <h3>Is Udemy’s 2022 Web Developer BootCamp Corse by Colt Steele Worth it?</h3>
+        <p>As I have said this is one of the most comprehensive courses to learn web development and Colt has also completely overhauled this course with new videos and lectures to make this up-to-date and I definitely think its worth of money, but, let’s review the course in a structured way considering instructor reputed and teaching style, course structure, and content quality, as well as what other people are saying about this course.</p>
+        <p> <b> 1. Instructor Reputation</b><br> There is no doubt that Colt Steele is one of the most popular Udemy instructors. He is from Bootcamps and that shows in his teaching style. He has a knack for simplifying complex concepts and explaining them with non-trivial examples.</p>
+        <p>No wonder, he has almost 1 million students enrolled on Udemy and around 11 courses in different specializations like web development, MySQL databases, Python, JavaScrip, and many more topics. His student graduates found jobs in big tech companies like Google and Square.</p>
+        <p>He is one of the most amazing instructors on Udemy and getting his courses on throwaway prices like $9.9 on Udemy is the best thing that can happen for any learner. Given he is a Bootcamp instructor, his teaching style is like the one you get on that 10,000$ Bootcamp but with a fraction of the price.</p>
+        <p>I really like his clarity and clean way to explain concepts. He has an amazing voice and explanation style which makes learning engaging and easy. You can learn more about him on his Udemy profile.</p>
+        <p>You should also take a look at these courses, most of them are bestsellers with thousands of students enrolled in them, which speaks loud about Colt Steele’s popularity as an online instructor.</p>
+
+        <p><b>2. Course Structure and Content Quality</b><br>Enough talking about the background for this instructor and let’s move to what more important which is the course content and what you will expect to learn when enrolling so let’s start exploring the content:</p>
+
+        <p><b>2.1. HTML/CSS</b><br>Those two technologies are the most used nowadays to create web pages for any website. HTML is used to create the structure or the layout of the web pages you see on your browser screen while CSS is used to customize the colors and the text of the HTML tags. This section will introduce you to these two technologies as a beginner until being an advanced user and creating many examples and projects.</p>
+        <section class="about">
+            <div class="image">
+                <img src="images/shtmlcss.jpg" alt="" class="htmlcssjsphoto">
+            </div>
+        </section>
+
+        <p><b>2.2. JavaScript</b><br>After learning the essential part of web development which is HTML/CSS you have now to learn JavaScript which will add a better user experience for your website like interaction and make the web pages more dynamic. Most of the current websites are utilizing JavaScript for these goals so it is a great thing to learn if you want to be a web developer.</p>
+        <section class="about">
+            <div class="image">
+                <img src="images/jsw.png" alt="" class="htmlcssjsphoto">
+            </div>
+        </section>
+
+        <p><b>2.3. jQuery</b><br>JavaScript is a really powerful programming language to make interactive websites but a little bit difficult to work with so developers have created a framework called jQuery to simplify the HTML DOM tree and also you can do the same functionality to your website that JavaScrit does with less code when using jQuery.</p>
+        <section class="about">
+            <div class="image">
+                <img src="images/jquery2.png" alt="" class="htmlcssjsphoto">
+            </div>
+        </section>
+
+        <p><b>2.4. Back-end</b><br>After learning the front-end and how designing dynamic and interactive web pages it is now the time for creating the back-end and what happened behind the scene when someone signs in or use your service.</p>
+        <section class="about">
+            <div class="image">
+                <img src="images/backend2.png" alt="" class="htmlcssjsphoto">
+            </div>
+        </section>
+
+        <p><b>2.5. Databases Technology</b><br>As I mentioned before that the full-stack web developer needs to learn three things designing the front-end, back-end, and database and this section is all about databases. You will learn how to create a NoSQL database using MongoDB and link it with your application.</p>
+        <section class="about">
+            <div class="image">
+                <img src="images/dbmsw.png" alt="" class="htmlcssjsphoto">
+            </div>
+        </section>
+
+
+        <p>Finally, you are going to deploy the web application on the Heroku service.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fa-brands fa-stack-overflow"></i>
+                <span>Bootcamp</span>
+            </div>
+        </div>
+   </div>
+</section>
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
