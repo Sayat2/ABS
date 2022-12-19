aa1f4a2991d5d174b2e1cb466254de4d81fd274f
Aidana <210103405@stu.sdu.edu.kz>
2022-12-20 00:35:46 +0600

    add new js_page file

--- js_page.php
+++ js_page.php
@@ -5,20 +5,12 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>ABS Education</title>
-
-  
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
    <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
-
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
-
-
 <section class="header">
 
    <a href="home.php" class="logo">ABS Education School</a>
@@ -85,41 +77,41 @@
 
 <section class="footer">
 
-   <div class="box-container">
+<div class="box-container">
 
-      <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
-         <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
-      </div>
+<div class="box">
+   <h3>quick links</h3>
+   <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+   <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+   <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+   <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+</div>
 
-      <div class="box">
-         <h3>extra links</h3>
-         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
-      </div>
+<div class="box">
+   <h3>extra links</h3>
+   <a href="https://mail.google.com/mail/u/1/#inbox?compose=new"> <i class="fas fa-angle-right"></i> ask questions</a>
+   <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+   <a href="https://policies.google.com/privacy?hl=en-US"> <i class="fas fa-angle-right"></i> privacy policy</a>
+   <a href="https://termly.io/legal-dictionary/terms-of-use/#:~:text=Terms%20of%20use%20are%20the,or%20%E2%80%9Cterms%20and%20conditions%E2%80%9D."> <i class="fas fa-angle-right"></i> terms of use</a>
+</div>
 
-      <div class="box">
-         <h3>contact info</h3>
-         <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
-         <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
-         <a href="#"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
-         <a href="#"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
-      </div>
+<div class="box">
+   <h3>contact info</h3>
+   <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+   <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+   <a href="https://mail.google.com/mail/u/1/#inbox?compose=new"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+   <a href="https://e.mail.ru/compose/"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+</div>
 
-      <div class="box">
-         <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-      </div>
+<div class="box">
+   <h3>follow us</h3>
+   <a href="https://www.instagram.com/whk_cd/"> <i class="fab fa-instagram"></i> instagram of Sayat</a>
+   <a href="https://www.instagram.com/bayan.sullu/"> <i class="fab fa-instagram"></i> instagram of Bayan</a>
+   <a href="https://www.instagram.com/awesomeaydana/"> <i class="fab fa-instagram"></i> instagram of Aidana</a>
+   <a href="https://www.instagram.com/sdukz/"> <i class="fab fa-instagram"></i> instagram of SDU</a>
+</div>
 
-   </div>
+</div>
 
    <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
 
