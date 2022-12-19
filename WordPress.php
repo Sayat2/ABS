91f4d2ff90d6533e2b9e235855919ea718db185d
Aidana <210103405@stu.sdu.edu.kz>
2022-12-20 00:48:59 +0600

    add WordPress file

---
+++ WordPress.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ABS Education</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <script src="https://kit.fontawesome.com/262b966825.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<section class="header">
+
+   <a href="home.php" class="logo">ABS Education School</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="buy.php">buy</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>WordPress</h1>
+</div>
+
+<section class="about">
+   <div class="image">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/zxXXC-vqWng" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+   <div class="content">
+      <h3>WordPress</h3>
+      <p>Welcome to the ABS Education School course!</p>
+      <p>What Is WordPress? A Beginner's Guide</p>
+   </div>
+</section>
+
+<section class="about">
+   <div class="content">
+        <h3>Introduction</h3>
+      <p>Creating a new website is a daunting task. You want to build something your customers will love, but you don’t want the setup to take several months or be so technical that you need a developer for every single copy update.</p>
+      <p>In your research, you’ve probably come across WordPress as a possible solution. But there’s one issue — you have no clue what WordPress is or how it can help your business. No worries, this beginner’s guide will teach you everything you need to know.</p>
+
+      <br><br><br><br>
+      <h3>What Is WordPress?</h3>
+      <p>WordPress is an open-source content management system (CMS). It’s a popular tool for individuals without any coding experience who want to build websites and blogs. The software doesn’t cost anything. Anyone can install, use, and modify it for free.</p>
+      <p>In the beginning, WordPress was mainly used to create blogs. Fast-forward to today, the software has improved, and you can create any type of website you want. You can build hobby or lifestyle blogs, professional portfolios, business websites, e-commerce stores, mobile applications, and membership sites.</p>
+
+      <br><br><br><br>
+      <h3>Hobby or Lifestyle Blogs</h3>
+      <p>Due to WordPress's origin in the blog space, its features for this purpose are rich, including:</p>
+      <p>1. The ability to differentiate between posts and pages</p>
+      <p>2. A robust WYSIWYG editor for easy post creation</p>
+      <p>3. Posts, tags, and categories for creating a rich content organization system</p>
+      <p>4. Options for users to leave comments and you to easily moderate them</p>
+      <br><br>
+      <p>Once you have WordPress installed, the main thing you'll want to do is choose a theme that appeals to you and what you want to accomplish with your blog. There are themes that accommodate news, magazine, and tiled layouts (and more).</p>
+      <p>Next, you'll want to add plugins for any functionality you want that doesn't come with the theme, such as social sharing buttons or click-to-tweet.</p>
+      <br><br><br><br>
+      <h3>Professional Portfolios</h3>
+      <p>You can also find a variety of themes for professional portfolios, making WordPress a great choice for showcasing your personal brand. The "Pages" functionality can be used for your homepage, About page, contact page, etc. while the "Posts" functionality can be used for the specific pieces you want to highlight — whether they're in image, text, audio, or video format.</p>
+      <p>You can choose a theme with a built-in gallery, or you can install plugins for portfolio carousels or sliders.</p>
+      <br><br><br><br>
+      <h3>Business Websites</h3>
+      <p>A business website needs to get even more mileage out of WordPress, in many cases having more pages and posts than the average personal blog or site and needing additional functionality. However, due to its powerful built-in CMS capabilities and the open-source community continuing to iterate solutions, WordPress is fantastic at meeting those needs.</p>
+      <p>You can choose a website theme that works best for what you're looking to accomplish and then customize it according to your brand. Page builders such as Elementor and Divi make the layout process even easier.</p>
+      <p>With plugins for SEO, lead capture, analytics, and more, you'll be able to integrate and grow your business using WordPress.</p>
+      <br><br><br><br>
+      <h3>E-Commerce Stores</h3>
+      <p>WordPress is particularly good for creating online stores. With e-commerce plugins such as WooCommerce, you can manage products, payments, and shipping in the back end while your visitors have an enjoyable shopping experience on the front end.</p>
+      <br><br><br><br>
+      <h3>Mobile Applications</h3>
+      <p>It can be far more convenient for some users to access your site's features through a mobile app rather than their device's browser. App development can be costly, but WordPress plugins can turn your site into an app with no hassle.</p>
+      <br><br><br><br>
+      <h3>Membership Sites</h3>
+      <p>If you're looking to monetize your efforts by restricting premium content to members only, WordPress supports plugins to create membership sites. You'll be able to manage payments, set up member levels, and monitor signups.</p>
+      <p>Want to know more about building a WordPress website? Check out this ultimate guide to WordPress.</p>
+   </div>
+</section>
+
+   <section class="about">
+   <div class="image">
+      <img src="images/wordpress.png" alt="" class="htmlcssjsphoto">
+   </div>
+   </section>
+
+   <section class="about">
+   <div class="content  ">
+        <h3>Why Use WordPress?</h3>
+        <p>WordPress is the beloved platform for many bloggers and businesses. In fact, more than 40% of all websites online use WordPress. Here are a few reasons why WordPress is so popular:</p>
+        
+        <br><br>
+        <h3>1. WordPress is open-source.</h3>
+        <p>WordPress is an open-source platform. This means anyone can modify the source code and redistribute the software. It’s designed for people to contribute their ideas to improve the platform. There’s an open-source community that helps make WordPress even better.</p>
+        <br><br><br><br>
+        <h3>2. WordPress is easy to use for a beginner.</h3>
+        <p>WordPress doesn’t require a computer engineering degree. It’s actually built for non-technical people. The user interface is intuitive and friendly. To add a new design to your website, you can simply download a theme. If you need to add a new function, like a contact form, you just install a plugin. Web development tasks that used to take hours to complete can be done in a matter of minutes.</p>
+        <br><br><br><br>
+        <h3>3. WordPress is SEO-friendly.</h3>
+        <p>It’s not enough to just build a WordPress website. You want people to find and visit your website through search engine optimization. With SEO plugins, like Yoast SEO, you can boost your rankings with internal linking suggestions and meta description templates.</p>
+        <br><br><br><br>
+        <h3>4. WordPress supports all media types.</h3>
+        <p>WordPress doesn’t limit any major media types. So, you can use any combination of text, images, and videos on your website. In this competitive market for consumers’ attention, it’s good to know that the platform gives you creative freedom.</p>
+        <br><br><br><br>
+        <h3>5. WordPress is secure.</h3>
+        <p>Security is a major concern for businesses. With the number of data breaches happening every year, you want peace of mind that your data (and your customers’ data) will be protected. As long as you keep plugins updated and passwords secure, WordPress is one of the safest and secure CMS platforms. The platform offers several features to safeguard your website, like logging out idle users and adding two-factor authentication.</p>
+        <br><br><br><br>
+        <h3>6. WordPress is free to use.</h3>
+        <p>WordPress is completely free. It won’t cost you anything to use the software. However, you will need to pay a third-party hosting provider to make your website available online.</p>
+        <br><br><br><br>
+        <h3>When Did WordPress Start?</h3>
+        <p>In 2003, WordPress was created by Matt Mullenweg, a college student at the time. It started as a relaunch of a previous project called b2/cafelog, originally built by Michel Valdrighi. Matt saw an opportunity to improve the functionality of a website without modifying its core source code. And WordPress was born.</p>
+        <br><br><br><br>
+        <h3>The Importance of WordPress 1.0</h3>
+        <p>Matt developed a new branch of b2 on SourceForge by forking the original concept. With the help of his friends, he created another version and got the name WordPress.</p>
+        <p>Several iterations later, WordPress 1.0 was released to the public in 2004. It was codenamed “Davis” because Matt admired jazz artists, like Miles Davis. Similar to the platform today, this version included multiple categories to group your website content and comment moderation.</p>
+        <br><br><br><br>
+        <h3>The Rise of WordPress</h3>
+        <p>WordPress gained traction after a competitor changed its pricing structure. Users started looking for alternative solutions, like WordPress. After quitting his job, Matt started his own company Automattic. He raised $1.10 million in Series A funding from multiple investors, including True Ventures, Radar Partners, and Polaris Ventures.</p>
+        <br><br><br><br>
+        <h3>The WordPress of Today</h3>     
+        <p>WordPress is now a growing community of developers, designers, and bloggers. In 2017, WordPress redesigned its user experience with a new editor called Gutenberg. The latest version, WordPress 5.0 (Bebo), includes a new and improved showcase theme. Major industries continue to see the value of the platform.</p>   
+        <br><br><br><br> <br><br><br><br>
+        <h3>Resources to Build Your WordPress Website</h3>
+        <p>It’s easy to get overwhelmed when building your website. There are so many options when it comes to hosting providers, page builders, and plugins. So, we narrowed down the list to get you started.</p>
+        <p>And once you've decided which of the resources listed below is best for your business, check out this video of five tips for building your Wordpress Website:</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/A3CmzNyGze8" title="5 Tips for Building a WordPress Website (Guide)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p>• WP Engine provides managed WordPress hosting for your website. Built for agencies, enterprises, and small businesses, this company offers a secure and reliable service.</p>
+        <p>• WPMU Dev is an all-in-one WordPress platform. You get everything from hosting to security protection to marketing tools to help you run a successful website.</p>
+        <p>• All in One SEO is a complete SEO plugin and toolkit for WordPress. With its TruSEO score and actionable checklist, you can easily optimize every page and post on your website for higher search engine rankings. </p>
+        <p>• WooCommerce helps you build an online business easier. This e-commerce platform includes unrestricted customization and built-in blogging to help your business grow.</p>
+        <p>• Elementor boasts more than 3 million websites using its WordPress page builder. With no technical skills, you can design stunning web pages from pre-made templates.</p>
+        <p>• GravityForms lets you create advanced forms with its visual editor. Convert more visitors with its conditional logic feature that configures your forms to show or hide fields based on the visitors’ selections.</p>
+        <p>• WP Beginner is a free WordPress resource for beginners. Their blog offers advice and tips on how to improve your website.</p>
+        <p>• The Events Calendar family of WordPress tools gives users free and paid options for calendars, RSVP and ticketing, and comprehensive events management.</p>
+        <p>Don’t be intimidated by the website creation process. While many website tools exist, WordPress offers you the flexibility to create an amazing online presence for your business.</p>
+        <p><i>Editor's note: This post was originally published in May 2020 and has been updated for comprehensiveness.</i></p>
+        <div class="image">
+            <img src="images/wom.jpg" alt="" class="htmlcssjsphoto">
+        </div>
+
+   </div>
+</section>
+
+
+
+<section class="footer">
+
+<div class="box-container">
+
+<div class="box">
+   <h3>quick links</h3>
+   <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+   <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+   <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+   <a href="buy.php"> <i class="fas fa-angle-right"></i> book</a>
+</div>
+
+<div class="box">
+   <h3>extra links</h3>
+   <a href="https://mail.google.com/mail/u/1/#inbox?compose=new"> <i class="fas fa-angle-right"></i> ask questions</a>
+   <a href="about.php"> <i class="fas fa-angle-right"></i> about us</a>
+   <a href="https://policies.google.com/privacy?hl=en-US"> <i class="fas fa-angle-right"></i> privacy policy</a>
+   <a href="https://termly.io/legal-dictionary/terms-of-use/#:~:text=Terms%20of%20use%20are%20the,or%20%E2%80%9Cterms%20and%20conditions%E2%80%9D."> <i class="fas fa-angle-right"></i> terms of use</a>
+</div>
+
+<div class="box">
+   <h3>contact info</h3>
+   <a href="#"> <i class="fas fa-phone"></i> +7-747-456-7890 </a>
+   <a href="#"> <i class="fas fa-phone"></i> +7-700-222-3333 </a>
+   <a href="https://mail.google.com/mail/u/1/#inbox?compose=new"> <i class="fas fa-envelope"></i> id@stu.sdu.edu.kz </a>
+   <a href="https://e.mail.ru/compose/"> <i class="fas fa-map"></i> Almaty, Kazakhstan - 050000 </a>
+</div>
+
+<div class="box">
+   <h3>follow us</h3>
+   <a href="https://www.instagram.com/whk_cd/"> <i class="fab fa-instagram"></i> instagram of Sayat</a>
+   <a href="https://www.instagram.com/bayan.sullu/"> <i class="fab fa-instagram"></i> instagram of Bayan</a>
+   <a href="https://www.instagram.com/awesomeaydana/"> <i class="fab fa-instagram"></i> instagram of Aidana</a>
+   <a href="https://www.instagram.com/sdukz/"> <i class="fab fa-instagram"></i> instagram of SDU</a>
+</div>
+
+</div>
+
+   <div class="credit"> created by team <span>Legends of Jaksylyk</span> </div>
+
+</section>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
